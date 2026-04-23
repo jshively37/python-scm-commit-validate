7a3dd69e4297006399ac2d3b808728b184408788
Jeff Shively <jshively@paloaltonetworks.com>
2026-04-23 13:50:45 -0400

    Adding global variable for sleep and making screen output more intuitive.

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ AUTH_HEADERS = {
     "Accept": "application/json",
 }
 
+# Time to sleep between requests in seconds.
+SLEEP_TIME = 15
+
 load_dotenv()
 TSG_ID = os.environ.get("TSG_ID")
 CLIENT_ID = os.environ.get("CLIENT_ID")
@@ -38,22 +41,19 @@ def create_token():
 def make_commit():
     url = f"{BASE_API_URL}/config/operations/v1/config-versions/candidate:push"
     payload = {"folders": ["All"], "description": "Commit from API"}
-    response = requests.request(
+    return requests.request(
         method="POST", url=url, headers=HEADERS, json=payload
     ).json()
-    return response
 
 
 def get_commit_jobs():
     url = f"{BASE_API_URL}/config/operations/v1/jobs"
-    response = requests.request(method="GET", url=url, headers=HEADERS).json()
-    return response
+    return requests.request(method="GET", url=url, headers=HEADERS).json()
 
 
 def get_specific_job(job_id):
     url = f"{BASE_API_URL}/config/operations/v1/jobs/{job_id}"
-    response = requests.request(method="GET", url=url, headers=HEADERS).json()
-    return response
+    return requests.request(method="GET", url=url, headers=HEADERS).json()
 
 
 if __name__ == "__main__":
@@ -64,16 +64,16 @@ if __name__ == "__main__":
     parent_job = commit_response["job_id"]
     status_payload = get_specific_job(job_id=parent_job)
     status = status_payload["data"][0]["result_str"]
-    print(f"Parent job status: {status}")
+    print(f"Parent job {parent_job} status: {status}")
     print("-" * 50)
 
     # Monitor the parent job until it is no longer pending.
     while status == "PEND":
+        sleep(SLEEP_TIME)
         status_payload = get_specific_job(job_id=commit_response["job_id"])
         status = status_payload["data"][0]["result_str"]
-        print(status)
+        print(f"Parent job {parent_job} status: {status}")
         print("-" * 50)
-        sleep(15)
 
     # Get all child jobs that are associated with the parent job and create a list of their IDs.
     child_jobs = []
@@ -82,15 +82,16 @@ if __name__ == "__main__":
         child_jobs.extend(
             x["id"] for x in all_jobs["data"] if x["parent_id"] == parent_job
         )
+
     # Loop through the child jobs and monitor their status until they are all complete.
     while child_jobs != []:
         for job in sorted(child_jobs):
             status_payload = get_specific_job(job_id=job)
             status = status_payload["data"][0]["result_str"]
-            print(f"Job {job} has status: {status}")
+            print(f"Child job {job} status: {status}")
             print("-" * 50)
             if status != "OK":
-                sleep(15)
+                sleep(SLEEP_TIME)
                 break
             else:
                 child_jobs.remove(job)
